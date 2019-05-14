9cf5b7c740c0399b1db2e1e54a6b709b1a4a0483
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-05-14 16:15:44 +0100

    added doses controller and crud

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,34 @@
 class DosesController < ApplicationController
+  # before_action :find_dose, only: [:show, :edit, :update, :destroy]
+
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # raise
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  # def find_dose
+  #   @dose = Dose.find(params[:id])
+  # end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
