2301175abdcdbeb1974838c10d8999da1860b20c
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-05-14 15:33:27 +0100

    added cocktails controller and crud views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+# how to use the cocktail db
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# serialized = open(url).read
+# ingredients = JSON.parse(serialized)
+
+# ingredients.each do |ingredient|
+#   puts ingredient
+# end
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "French 75")
+Cocktail.create(name: "White Russian")
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "champagne")
