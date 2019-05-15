cfb2a6d7afe68bd2e495d3115a3b011863587ff3
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-05-15 10:54:05 +0100

    Importing ingredients as seeds from cockatil db api

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,17 @@ require 'open-uri'
 Ingredient.destroy_all
 Cocktail.destroy_all
 
-# how to use the cocktail db
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized = open(url).read
+ingredients = JSON.parse(serialized)
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# serialized = open(url).read
-# ingredients = JSON.parse(serialized)
 
-# ingredients.each do |ingredient|
-#   puts ingredient
-# end
-#
+
+ingredients["drinks"].each do |ingredient|
+
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
 
 Cocktail.create(name: "Manhattan")
 Cocktail.create(name: "Mojito")
@@ -21,7 +22,7 @@ Cocktail.create(name: "French 75")
 Cocktail.create(name: "White Russian")
 
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "champagne")
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "champagne")
